77d81c94e88790b43a8b0303e1e08ac93f14fe28
mastertar <78391289+mastertar@users.noreply.github.com>
2022-02-03 15:10:12 -0600

    Update 2.0
    This update now allows you to convert from base 10 to any other base, as well as fixes some other small issues.

--- BaseConverter.java
+++ BaseConverter.java
@@ -1,25 +1,65 @@
 import java.lang.Math;
 import java.util.Scanner;
-public class BaseConvertor 
+public class BaseConverter 
 {
 	public static void main(String[] args)
 	{
 		Scanner input = new Scanner(System.in);
-        System.out.println("What is the base for your number?");
-        int base = input.nextInt();
-        System.out.println("What is your number?");
-        int number = input.nextInt();
-        while(number != -1)
-        {
-            convert(base, number);
-            System.out.println("What is the base for your number?");
-            base = input.nextInt();
-            System.out.println("What is your number? (-1 to quit)");
-            number = input.nextInt();
-        }
+		
+		boolean firstRun = true;
+		while(true)
+		{
+    		System.out.println("Please choose an option:\n1 - From any base to base 10\n2 - From base 10 to any base\n(-1 to quit)");
+    		if(firstRun == false)
+    		{
+    		input.nextLine();
+    		}
+    		firstRun = false;
+    		String option = input.nextLine();
+    		
+    		if(option.indexOf("1") == 0)
+    		{
+                System.out.println("Option 1 has been chosen.\n\nWhat is the base for your number?");
+                int base = input.nextInt();
+                System.out.println("What is your number?");
+                int number = input.nextInt();
+                while(number != -1)
+                {
+                    convertTo10(base, number);
+                    System.out.println("What is the base for your number?");
+                    base = input.nextInt();
+                    System.out.println("What is your number? (-1 to quit)");
+                    number = input.nextInt();
+                }
+    		}
+    		else if(option.indexOf("2") == 0)
+    		{
+    		    System.out.println("Option 2 has been chosen.\n\nWhat is the base you want your number to be in?");
+                int base = input.nextInt();
+                System.out.println("What is your number?");
+                int number = input.nextInt();
+                while(number != -1)
+                {
+                    convertFrom10(base, number);
+                    System.out.println("What is the base you want your number to be in?");
+                    base = input.nextInt();
+                    System.out.println("What is your number? (-1 to quit)");
+                    number = input.nextInt();
+                }
+    		}
+    		else if(option.indexOf("-1") == 0)
+    		{
+    		    System.out.println("Exiting Program.");
+    		    break;
+    		}
+    		else
+    		{
+    		    System.out.println("Please select a valid option!");
+    		}
+		}
 	}
 
-    public static void convert(int base, int number)
+    public static void convertTo10(int base, int number)
     {
         if(base < 2)
         {
@@ -37,11 +77,31 @@ public class BaseConvertor
                 return;
             }
             sum += ((temp % 10) * (Math.pow(base, counter)));
-            // temp -= (temp%10);
-            // temp /= 10;
             temp = (temp - (temp%10)) / 10;
             counter++;
         }
         System.out.println(number + " base " + base + " is " + sum);
+    }
+    public static void convertFrom10(int base, int number)
+    {
+        if(number < 0)
+        {
+            System.out.println("Please enter a valid number!");
+            return;
+        }
+        int temp = number;
+        String finalNum = "";
+        while(temp != 0)
+        {
+            finalNum += temp % base;
+            temp = (temp - (temp % base))/base;
+        }
+        String tempString = finalNum;
+        finalNum = "";
+        for(int i = tempString.length()-1; i>-1; i--)
+        {
+            finalNum += tempString.substring(i, i+1);
+        }
+        System.out.println(number + " is " + finalNum + " base " + base);
     }
 }
